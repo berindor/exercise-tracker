527b577aacab4e6e4eb08b5b9acb0b1d7749b607
Beringer Dorottya <beringerdorottya@gmail.com>
2024-02-14 16:18:00 +0100

    added list users, remove,  create exercise small ui fixes

--- index.js
+++ index.js
@@ -38,17 +38,50 @@ let Users = mongoose.model('User', userSchema);
 
 let Exercises = mongoose.model('Exercise', exerciseSchema);
 
-app.post('/api/users', urlencodedParser, async (req, res) => {
-  if (!req.body.username) {
-    res.json({ error: 'You should add a non-empty username.' });
-  } else {
-    let user = await Users.findOne({ username: req.body.username });
-    if (!user) {
-      await Users.create({ username: req.body.username });
-      user = await Users.findOne({ username: req.body.username });
-      res.json(user);
+app
+  .route('/api/users')
+  .post(urlencodedParser, async (req, res) => {
+    if (!req.body.username) {
+      res.json({ error: 'You should add a non-empty username.' });
     } else {
-      res.json({ 'User already exists': user });
+      let user = await Users.findOne({ username: req.body.username });
+      if (!user) {
+        await Users.create({ username: req.body.username });
+        user = await Users.findOne({ username: req.body.username });
+        res.json(user);
+      } else {
+        res.json({ 'User already exists': user });
+      }
+    }
+  })
+  .get(async (req, res) => {
+    const userList = await Users.find({});
+    res.json({ users: userList });
+    return userList;
+  });
+
+app.post('/api/users/:userid/exercises', urlencodedParser, async (req, res) => {
+  let user = await Users.findOne({ _id: req.params.userid });
+  if (!user) {
+    res.json({ error: 'User does not exist.' });
+  } else {
+    if (!req.body.description) {
+      res.json({ error: 'Description is mandatory.' });
+    } else if (!req.body.duration) {
+      res.json({ error: 'Duration is mandatory.' });
+    } else {
+      const date = req.body.date; //need to be Date format and now, if empty
+      await Exercises.create({ userid: req.params.userid, description: req.body.description, duration: req.body.duration, date });
     }
   }
+});
+
+//app.get('api/users/:userid/...')
+//({username: '', description: '', duration: '', date: '', userid: ''});
+
+//to remove test data
+app.get('/api/remove/tests', async (req, res) => {
+  const nameToRemove = /^fcc/;
+  const deleted = await Users.deleteMany({ username: nameToRemove });
+  res.json({ removed: `${deleted.deletedCount} users` });
 });
